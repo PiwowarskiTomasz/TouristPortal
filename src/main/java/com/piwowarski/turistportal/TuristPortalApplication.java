f80b3a3cc08aeabd0bb634ef51c2a5fe43d8d00a
Tomek <t.piwowarski17@gmail.com>
2018-02-27 13:56:34 +0100

    Initial commit

---
+++ src/main/java/com/piwowarski/turistportal/TuristPortalApplication.java
@@ -0,0 +1,12 @@
+package com.piwowarski.turistportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristPortalApplication.class, args);
+	}
+}
